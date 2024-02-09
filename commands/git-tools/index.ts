07d66133e5ff0d67caf4959b3f28e0487f383eaf
Jonathan Delgado <hi@jon.soy>
2024-02-09 18:17:35 -0300

    feat: implement git-assistant enhancements for token setup, commit and PR message generation
    This commit introduces significant enhancements to the git-assistant tool, focusing on user experience and functionality improvements. It starts by updating the workflow for setting up the OpenAI token, ensuring that users can easily configure their token for authenticating with the OpenAI API. Alongside, the commit introduces the ability to generate commit messages and PR messages utilizing OpenAI's GPT models, aimed at assisting users in crafting more meaningful and comprehensive commit messages based on their git diffs. To facilitate these features, changes include:
    
    1. Modification of file paths to centralize configuration and token storage under the user's home directory, improving security and organization.
    2. Implementation of functions to check for and retrieve stored OpenAI tokens, enhancing user experience by avoiding repetitive token entry.
    3. Addition of YAML dependency to handle configuration storage, enabling easy maintenance and extension of git-assistant's capabilities.
    4. Creation of new commands `init` for token setup, `get-commit-message` for generating commit messages, and `get-pr-message` for generating PR messages, each utilizing OpenAI's AI models for content generation.
    5. Updates to command-line interface help messages, ensuring clear guidance for users on the new features and their usage.
    
    Together, these enhancements make git-assistant a more powerful and user-friendly tool for developers, leveraging AI to streamline the code versioning process.

--- commands/git-tools/index.ts
+++ commands/git-tools/index.ts
@@ -1,86 +1,221 @@
-import { command, flag, flags, isStringAt, makeHelmMessage, restArgumentsAt, rule, type Rule } from "@jondotsoy/flags";
-import { inspect, pathToFileURL, readableStreamToArrayBuffer, readableStreamToText } from "bun";
+import {
+  command,
+  flag,
+  flags,
+  isBooleanAt,
+  isStringAt,
+  makeHelmMessage,
+  restArgumentsAt,
+  rule,
+  type Rule,
+} from "@jondotsoy/flags";
+import {
+  inspect,
+  pathToFileURL,
+  readableStreamToArrayBuffer,
+  readableStreamToText,
+} from "bun";
 import { homedir } from "os";
 import { OpenAI } from "openai";
 import { writeFile, mkdir, readFile } from "fs/promises";
 import chalk from "chalk";
 import type { MessageContentText } from "openai/resources/beta/threads/messages/messages.mjs";
+import { existsSync } from "fs";
+import * as YAML from "yaml";
+
+class ErrorNotFound extends Error {}
 
 const sleep = async (t: number) => {
-  const { promise, resolve, reject } = Promise.withResolvers<void>()
+  const { promise, resolve, reject } = Promise.withResolvers<void>();
 
   setTimeout(resolve, t);
 
-  await promise
-}
+  await promise;
+};
 
 type Ctx = {
   stdout: WritableStream;
   stderr: WritableStream;
 };
 
-const HomeLocation = pathToFileURL(`${homedir()}/`);
-const OpenAIApiKeyLocation = new URL(`.openai/auth.token`, HomeLocation);
+const HomeLocation = pathToFileURL(`${homedir()}/.git-assistant/`);
+const OpenAIApiKeyLocation = new URL(`openai.apikey`, HomeLocation);
+const OpenAIAssistantsLocation = new URL(`openai.assistants`, HomeLocation);
+
+const setOpenAIToken = async (token: string) => {
+  await writeFile(OpenAIApiKeyLocation, token, { mode: 0o600 });
+};
+
+const getOpenAIToken = async () => {
+  if (!existsSync(OpenAIApiKeyLocation))
+    throw new ErrorNotFound(`Missing OpenAI api key`);
+
+  return new TextDecoder().decode(await readFile(OpenAIApiKeyLocation));
+};
 
 const getOpenAI = async () => {
-  const apiKey = new TextDecoder().decode(await readFile(OpenAIApiKeyLocation))
-  const openAI = new OpenAI({ apiKey })
+  const apiKey = await getOpenAIToken();
+  const openAI = new OpenAI({ apiKey });
   return openAI;
-}
+};
 
-const gitDiff = async () => {
+type Assistants = {
+  commitMessage: string;
+  PRMessage: string;
+};
+
+const setConfigAssistant = async (assistants: Assistants) => {
+  await writeFile(OpenAIAssistantsLocation, YAML.stringify(assistants));
+};
+
+const getConfigAssistant = async () => {
+  if (!existsSync(OpenAIAssistantsLocation))
+    throw new ErrorNotFound(
+      `Missing assistant to Open AI.\nPlease configure your assistant with ${chalk.green(`git-assistant init`)}.`,
+    );
+
+  const assistants = YAML.parse(
+    new TextDecoder().decode(await readFile(OpenAIAssistantsLocation)),
+  );
+
+  return assistants as Assistants;
+};
+
+const gitDiffBranchFromMaster = async (options?: { mainBranch?: string }) => {
+  const mainBranch = options?.mainBranch ?? "master";
+
   const childProcess = Bun.spawn({
-    cmd: ['git', '--no-pager', 'diff', '--staged'],
-    stdout: 'pipe',
-    stderr: 'pipe',
-  })
+    cmd: ["git", "--no-pager", "diff", `${mainBranch}..HEAD`],
+    stdout: "pipe",
+    stderr: "pipe",
+  });
 
-  return await readableStreamToText(childProcess.stdout)
-}
+  return await readableStreamToText(childProcess.stdout);
+};
 
-const sign = async (args: string[]) => {
+const gitDiffStaged = async () => {
+  const childProcess = Bun.spawn({
+    cmd: ["git", "--no-pager", "diff", "--staged"],
+    stdout: "pipe",
+    stderr: "pipe",
+  });
+
+  return await readableStreamToText(childProcess.stdout);
+};
+
+const init = async (args: string[]) => {
   type Options = {
-    token: string
+    token: string;
   };
   const rules: Rule<Options>[] = [
-    rule(flag('--token', '-t'), isStringAt('token'), { description: 'Token of openai' })
+    rule(flag("--token", "-t"), isStringAt("token"), {
+      description: "Token of openai",
+    }),
   ];
   const options = flags<Options>(args, {}, rules);
 
-  if (!options.token) throw new Error(`Missing --token flag`)
+  const getOpenAITokenSafe = async () => {
+    try {
+      return await getOpenAIToken();
+    } catch (ex) {
+      if (ex instanceof ErrorNotFound) return null;
+      throw ex;
+    }
+  };
 
-  await mkdir(new URL('./', OpenAIApiKeyLocation), { recursive: true, mode: 0o700 })
-  await writeFile(OpenAIApiKeyLocation, options.token, { mode: 0o600 })
-  console.log(`Token stored`)
-}
+  const currentOpenAIToken = await getOpenAITokenSafe();
+  const requiredToken = currentOpenAIToken === null;
 
-const logDirLocation = new URL('.logs/requests/', import.meta.url)
-await mkdir(logDirLocation, { recursive: true })
-const logCounter = { counter: 0, nextCounter: () => (logCounter.counter++).toString().padStart(5, '0') }
+  if (!options.token && requiredToken) throw new Error(`Missing --token flag`);
 
-async function* simpleMessageToOpenAI(assistant_id: string, message_content: string) {
+  await mkdir(HomeLocation, { recursive: true, mode: 0o700 });
+
+  if (options.token) await setOpenAIToken(options.token);
+
+  const openAI = await getOpenAI();
+
+  const getConfigAssistantSafe = async () => {
+    try {
+      return await getConfigAssistant();
+    } catch (ex) {
+      if (ex instanceof ErrorNotFound) return null;
+      throw ex;
+    }
+  };
+
+  const currentAssistants = await getConfigAssistantSafe();
+
+  const assistantCommitMessage = currentAssistants?.commitMessage
+    ? await openAI.beta.assistants.retrieve(currentAssistants.commitMessage)
+    : await openAI.beta.assistants.create({
+        model: "gpt-4-turbo-preview",
+        name: "(git-assistant) Generator Commit Message by Diff",
+        instructions: `Please generate a commit message. Ensure that it includes a precise and informative subject line. If necessary, follow with an explanatory body providing insight into the nature of the changes, the reasoning behind them, and any significant consequences or considerations arising from them. Ensure the message begin with a conventional commit format. Please not split paragraph.`,
+      });
+
+  const assistantPRMessage = currentAssistants?.PRMessage
+    ? await openAI.beta.assistants.retrieve(currentAssistants.PRMessage)
+    : await openAI.beta.assistants.create({
+        model: "gpt-4-turbo-preview",
+        name: "(git-assistant) Generator RM Message by Diff",
+        instructions: `Se entregara un cuerpo donde la primera linea sera el numero de ticket, la segunda linea sera el principal documento en que se enfoca el cambio (Debe ser usado para describir los cambios) y desde la tercera linea se entrega diferencias de git. 
+
+Se debe entregar un document donde la primera linea sea un titulo en format conventional commit  y con el prefijo el nimero de ticket. Ej \`(FOO-30) feat: write good code\` en formato texto plano.
+
+Luego un salto de linea 
+
+Luego debe entregar el cuerpo donde la primera linea sea el ticket relacionado en formato markdown. Ej Ticket related: (FOO-30)[jiraurl]
+
+Luego un salto de linea 
+
+A continuación una sección Changes con el detallo de los cambios realizados, lo mas detallado posible en format markdown. Debe solo tener un titulo en todo el documento que diga "Changes" y un emoji al final del titulo.`,
+      });
+
+  await setConfigAssistant({
+    commitMessage: assistantCommitMessage.id,
+    PRMessage: assistantPRMessage.id,
+  });
+  console.log(`Store config assistants`);
+};
+
+// const logDirLocation = new URL('.logs/requests/', import.meta.url)
+// await mkdir(logDirLocation, { recursive: true })
+// const logCounter = { counter: 0, nextCounter: () => (logCounter.counter++).toString().padStart(5, '0') }
+
+async function* simpleMessageToOpenAI(
+  assistant_id: string,
+  message_content: string,
+) {
   const fnLog = async <T>(fn: () => Promise<T>): Promise<T> => {
-    const logLocationRequest = new URL(`${Date.now()}-${logCounter.nextCounter()}.req`, logDirLocation)
-    const logLocationResponse = new URL(`${Date.now()}-${logCounter.nextCounter()}.res`, logDirLocation)
-    await writeFile(logLocationRequest, `${fn.toString()}`)
-    const res = await fn()
-    await writeFile(logLocationResponse, `${inspect(res, { depth: Infinity })}`)
-    return res
-  }
+    // const logLocationRequest = new URL(`${Date.now()}-${logCounter.nextCounter()}.req`, logDirLocation)
+    // const logLocationResponse = new URL(`${Date.now()}-${logCounter.nextCounter()}.res`, logDirLocation)
+    // await writeFile(logLocationRequest, `${fn.toString()}`)
+    const res = await fn();
+    // await writeFile(logLocationResponse, `${inspect(res, { depth: Infinity })}`)
+    return res;
+  };
 
-  const openAI = await getOpenAI()
+  const openAI = await getOpenAI();
 
+  const thread = await fnLog(() => openAI.beta.threads.create());
 
-  const thread = await fnLog(() => openAI.beta.threads.create())
+  await fnLog(() =>
+    openAI.beta.threads.messages.create(thread.id, {
+      role: "user",
+      content: message_content,
+    }),
+  );
 
-  await fnLog(() => openAI.beta.threads.messages.create(thread.id, { role: 'user', content: message_content }))
-
-  const run = await fnLog(() => openAI.beta.threads.runs.create(thread.id, { assistant_id }))
+  const run = await fnLog(() =>
+    openAI.beta.threads.runs.create(thread.id, { assistant_id }),
+  );
 
   while (true) {
-    const { status } = await fnLog(() => openAI.beta.threads.runs.retrieve(thread.id, run.id))
+    const { status } = await fnLog(() =>
+      openAI.beta.threads.runs.retrieve(thread.id, run.id),
+    );
 
-    if (status === 'in_progress') {
+    if (status === "in_progress") {
       await sleep(500);
       continue;
     }
@@ -88,10 +223,12 @@ async function* simpleMessageToOpenAI(assistant_id: string, message_content: str
     break;
   }
 
-  const messages = await fnLog(() => openAI.beta.threads.messages.list(thread.id))
+  const messages = await fnLog(() =>
+    openAI.beta.threads.messages.list(thread.id),
+  );
 
   for await (const e of messages.iterPages()) {
-    yield* await fnLog(async () => e.getPaginatedItems())
+    yield* await fnLog(async () => e.getPaginatedItems());
   }
 }
 
@@ -100,56 +237,111 @@ const getCommitMessage = async (args: string[]) => {
   const rules: Rule<Options>[] = [];
   const options = flags<Options>(args, {}, rules);
 
-  const currentDiff = await gitDiff()
+  const currentDiff = await gitDiffStaged();
 
-  const assistant_id = 'asst_fw8RhO22vlGCAK6Hlz6HAyHn'
+  const assistant_id = (await getConfigAssistant()).commitMessage;
 
-  process.stderr.write(`${chalk.gray(`Loading...`)}\n`)
+  process.stderr.write(`${chalk.gray(`Loading...`)}\n`);
   const messages = await Array.fromAsync(
     simpleMessageToOpenAI(assistant_id, currentDiff),
-  )
+  );
 
   const value = messages
-    .filter((message) => message.role === 'assistant')
-    .map(message =>
-      message
-        .content
-        .filter((content): content is MessageContentText => content.type === 'text')
-        .map(e => e.text.value)
-        .join('\n')
+    .filter((message) => message.role === "assistant")
+    .map((message) =>
+      message.content
+        .filter(
+          (content): content is MessageContentText => content.type === "text",
+        )
+        .map((e) => e.text.value)
+        .join("\n"),
     )
-    .join('\n');
+    .join("\n");
 
-  process.stdout.write(value)
-}
+  process.stdout.write(value);
+};
+
+const getPRMessage = async (args: string[]) => {
+  type Options = {
+    mainBranch: string;
+    ticketRelated: string;
+  };
+  const rules: Rule<Options>[] = [
+    rule(flag("--main-branch"), isStringAt("mainBranch"), {
+      description: "Main branch. Default `master`",
+    }),
+    rule(flag("--ticket-related"), isStringAt("ticketRelated"), {
+      description: "Ticket related to create PR",
+    }),
+  ];
+  const options = flags<Options>(args, {}, rules);
+
+  const mainBranch = options.mainBranch ?? "master";
+  const ticketRelated = options.ticketRelated ?? "[No ticket]";
+
+  const currentDiff = await gitDiffBranchFromMaster({ mainBranch });
+
+  const assistant_id = (await getConfigAssistant()).PRMessage;
+
+  process.stderr.write(`${chalk.gray(`Loading...`)}\n`);
+  const messages = await Array.fromAsync(
+    simpleMessageToOpenAI(
+      assistant_id,
+      `${ticketRelated}\n\nNo definido.\n\n${currentDiff}`,
+    ),
+  );
+
+  const value = messages
+    .filter((message) => message.role === "assistant")
+    .map((message) =>
+      message.content
+        .filter(
+          (content): content is MessageContentText => content.type === "text",
+        )
+        .map((e) => e.text.value)
+        .join("\n"),
+    )
+    .join("\n");
+
+  process.stdout.write(value);
+};
 
 async function main(args: string[]) {
   type Options = {
-    sign: string[],
-    'getCommitMessage': string[],
+    init: string[];
+    getCommitMessage: string[];
+    getPRMessage: string[];
   };
   const rules: Rule<Options>[] = [
-    rule(command('sign'), restArgumentsAt('sign'), { description: 'Sign in with a token' }),
-    rule(command('get-commit-message'), restArgumentsAt('getCommitMessage'), { description: 'Create a commit message' }),
+    rule(command("init"), restArgumentsAt("init"), {
+      description: "Sign in with a OpenAI token",
+    }),
+    rule(command("get-commit-message"), restArgumentsAt("getCommitMessage"), {
+      description: "Create a commit message",
+    }),
+    rule(command("get-pr-message"), restArgumentsAt("getPRMessage"), {
+      description: "Create a PR commit message. Title, changes and references",
+    }),
   ];
   const options = flags<Options>(args, {}, rules);
-  const makeHelp = () => makeHelmMessage('cli', rules)
+  const makeHelp = () => makeHelmMessage("cli", rules);
 
-  if (options.sign) return sign(options.sign)
-  if (options.getCommitMessage) return getCommitMessage(options.getCommitMessage)
+  if (options.init) return init(options.init);
+  if (options.getCommitMessage)
+    return getCommitMessage(options.getCommitMessage);
+  if (options.getPRMessage) return getPRMessage(options.getPRMessage);
 
-  return console.log(makeHelp())
+  return console.log(makeHelp());
 }
 
 const bootstrap = async () => {
   await main(process.argv.slice(2));
 };
 
-await bootstrap()
-  .catch((ex) => {
-    if (ex instanceof Error) {
-      console.error(ex.message);
-      return;
-    }
-    throw ex;
-  });
+await bootstrap().catch((ex) => {
+  if (ex instanceof Error) {
+    console.error(ex.message);
+    return;
+  }
+  throw ex;
+});
